1f738f5dc789e343f44dde1551483c92de8cbc1d
Serhan Ertürk <srnerturk@gmail.com>
2020-01-10 16:13:52 +0300

    port added

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/todos', (req, res) => {
   res.send([{ id: 0, text: 'Hello World' }, { id: 1, text: 'Hello Js' }])
 })
 
-app.listen(5000, () =>
-  console.log(`Listening on port 5000.`),
+var port = process.env.PORT || 8000;
+app.listen(port, () =>
+  console.log(`Listening on port.`),
 );
