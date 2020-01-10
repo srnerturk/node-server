37315a7c6454c8c02d8c0dd044534d2305d2f5a8
Serhan Ertürk <srnerturk@gmail.com>
2020-01-10 16:10:09 +0300

    endpoint added

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 })
 
+app.get('/todos', (req, res) => {
+  res.send([{ id: 0, text: 'Hello World' }, { id: 1, text: 'Hello Js' }])
+})
+
 app.listen(5000, () =>
   console.log(`Listening on port 5000.`),
 );
